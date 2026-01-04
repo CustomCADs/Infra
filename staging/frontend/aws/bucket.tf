cf141228395a15fb53caad0a8ac7f59eec251cee
Ivan Angelov <ivanangelov414@gmail.com>
2026-01-04 19:00:45 +0200

    Added Staging AWS frontend

---
+++ staging/frontend/aws/bucket.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "customcads_bucket" {
+  bucket        = "customcads-staging-frontend"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "customcads_bucket_policy" {
+  bucket = aws_s3_bucket.customcads_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontOACRead"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.customcads_bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.customcads_cdn.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_ownership_controls" "customcads_bucket_ownership" {
+  bucket = aws_s3_bucket.customcads_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "customcads_bucket_oac" {
+  name                              = "customcads-staging-oac"
+  description                       = "OAC for ${aws_s3_bucket.customcads_bucket.bucket}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_s3_bucket_public_access_block" "customcads_access" {
+  bucket                  = aws_s3_bucket.customcads_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ staging/frontend/aws/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket                      = "customcads-terraform-staging"
+    key                         = "frontend/aws/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
