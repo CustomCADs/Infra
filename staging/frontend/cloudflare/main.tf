4ceb33e32d170cdbc9dc64ef644ef136adcd90db
Ivan Angelov <ivanangelov414@gmail.com>
2026-01-04 19:00:56 +0200

    Added Staging Cloudflare frontend

---
+++ staging/frontend/cloudflare/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket                      = "customcads-terraform-staging"
+    key                         = "frontend/cloudflare/terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.api_token
+}
